09c2ae109a7faa9dadec44d6408900057c7329ae
felipeamaral83 <felipeamaral83@yahoo.com.br>
2019-06-15 13:29:15 -0300

    Implementação campo minado

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './params'
-import Field from './components/Field'
+import MineField from './components/mineField'
+import { createMinedBoard } from './functions'
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Iniciando o Campo Minado!</Text>
-        <Text style={styles.instructions}>
-          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
-        </Text>
-        <Field />
-      </View>
-    )
-  }
+  
+	constructor(props) {
+		super(props)
+		this.state = this.createState()
+	}
+
+	minesAmount = () => {
+		const cols = params.getColumnsAmount()
+		const rows = params.getRowsAmount()
+		return Math.ceil(cols * rows * params.difficultLevel)
+	}
+
+	createState = () => {
+		const cols = params.getColumnsAmount()
+		const rows = params.getRowsAmount()
+		return {
+			board: createMinedBoard(rows, cols, this.minesAmount())
+		}
+  	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>Iniciando o Campo Minado!</Text>
+				<Text style={styles.instructions}>
+					Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+				</Text>
+				<View style={styles.container}>
+					<MineField board={this.state.board} />
+				</View>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center'
-  },
-  instructions: {
-    fontSize: 16,
-    textAlign: 'center'
-  }
+	container: {
+		flex: 1,
+		justifyContent: 'flex-end'
+	},
+	board: {
+		alignItems: 'center',
+		backgroundColor: '#AAA'
+	}
 });
